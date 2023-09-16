3accbcbdcb72a04f5d25ce190b8516d8e7ee3a55
Md Noble <mdnoble.wd@gmail.com>
2023-09-16 08:58:24 +0600

    created header section and added styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-5xl text-center my-12">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
