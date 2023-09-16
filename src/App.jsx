696e7cf2d9167170c55c2996d833394f9590b375
Md Noble <mdnoble.wd@gmail.com>
2023-09-16 18:59:55 +0600

    added cart section to show selected courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,77 @@
-import './App.css'
+import './App.css';
 import Header from './components/Header/Header';
+import Courses from './components/Courses/Courses';
+import Bookmarks from './components/Bookmarks/Bookmarks';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+  
+  const [bookMarks, setBookMarks] = useState([]); 
+  const [totalPrice, setTotalPrice] = useState(0); 
+  const [allTimes, setAllTimes] = useState(0); 
+  const [remaining, setRemaining] = useState(0); 
 
+  
+  const handleSelectedToCart = (course) => {
+    
+    const isExist = bookMarks.find(item => item.id === course.id);
+
+    let credit = course.time_hour;
+    let count = course.price;
+
+    if (isExist) {
+
+      return toast('You have already added'); 
+
+    } 
+    
+    else {
+      
+      bookMarks.forEach(item => {
+        count += item.price;
+      });
+
+      bookMarks.forEach(times => {
+        credit += times.time_hour;
+      });
+
+      
+      if (credit >= 20) {
+
+        return toast("You don't have enough credit balance");
+
+      } 
+      
+      else {
+        
+        const totalRemaining = 20 - credit;
+        setRemaining(totalRemaining);
+        setAllTimes(credit);
+        setTotalPrice(count.toFixed(2));
+
+        
+        const newBookMarks = [...bookMarks, course];
+        setBookMarks(newBookMarks);
+      }
+    }
+  }
+
+  
   return (
     <>
       <Header></Header>
-      <main>
-      </main>
+      <div className='flex'>
+        <Courses handleSelectedToCart={handleSelectedToCart}></Courses>
+        <Bookmarks bookMarks={bookMarks} allTimes={allTimes} remaining={remaining} totalPrice={totalPrice}></Bookmarks>
+      </div>
+      
+      <ToastContainer/>
     </>
   )
 }
 
-export default App
 
-
-
-
-
-
+export default App;
--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,51 +1,46 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
 import PropTypes from "prop-types";
 
-const Course = ({course}) => {
 
-    const { cover, title, description, price, time_hour } = course;
+const Course = ({ course, handleSelectedToCart }) => {
+  // Destructure properties from the 'Course' object
+  const { cover, title, description, price, time_hour } = course;
 
-    return (
-        <div>
-            <div className="w-80 bg-gray-100 p-4 rounded-lg drop-shadow-2xl">
-        {/* Display the blog cover image */}
+  return (
+    <div>
+      <div className="w-80 bg-gray-100 p-4 rounded-lg drop-shadow-2xl">
         <img
           className="mb-4 w-full"
           src={cover}
           alt=""
         />
-        {/* Display the blog title */}
         <h3 className="font-semibold text-lg mb-3">
           {title}
         </h3>
-        {/* Display the blog description */}
         <p className="text-gray-400 text-sm text-justify mb-4">
           {description}
         </p>
         <div className="flex justify-between items-center mb-6">
           <div>
-            {/* Display the blog price */}
             <p className="font-medium text-gray-400">Price : {price}</p>
           </div>
           <div>
-            {/* Display the blog credit hours */}
             <p className="font-medium text-gray-400">Credit : {time_hour}hr</p>
           </div>
         </div>
-        {/* Button to select the blog */}
         <button
+          onClick={() => handleSelectedToCart(course)}
           className="w-full bg-sky-500 py-2 rounded-md font-semibold text-lg text-white hover:bg-sky-400">
           Select
         </button>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
-Course.propTypes = {
-    course: PropTypes.object, 
-    handleSelectToBookMark: PropTypes.func,
-  };
 
-export default Course;
+Course.propTypes = {
+  course: PropTypes.object,
+  handleSelectedToCart: PropTypes.func, 
+};
+
+export default Course; 
