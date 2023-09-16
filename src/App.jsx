2a7fb96af9de891e6c3f6615ba34e156007ab413
Md Noble <mdnoble.wd@gmail.com>
2023-09-16 12:42:55 +0600

    added cart section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Courses from './components/courses/courses'
+import Cart from './components/Cart/Cart'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <main className='container mx-auto my-16'>
+      <main className='container mx-auto my-16 flex justify-between gap-6'>
         <Courses></Courses>
+        <Cart></Cart>
       </main>
     </>
   )
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div className=' bg-red-500'>
+            <h3>Course Name</h3>
+        </div>
+    );
+};
+
+export default Cart;
